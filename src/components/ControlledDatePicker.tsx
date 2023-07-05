9ea21e1d7a43585b1c20036ebbabbd656b10f7c6
igor coelho <igorc2@gmail.com>
2023-07-05 12:52:39 -0300

    adding transaction connection

--- src/components/ControlledDatePicker.tsx
+++ src/components/ControlledDatePicker.tsx
@@ -3,6 +3,7 @@ import { FieldValues, useController, UseControllerProps } from 'react-hook-form'
 import { FormLabel, FormControl } from "@chakra-ui/react";
 
 import { SingleDatepicker } from "chakra-dayzed-datepicker";
+import { ChangeEvent } from 'react';
 
 // interface IDatePickerWithValidation
 //   extends UseControllerProps<StudentFormData> {
@@ -18,12 +19,22 @@ interface IControlledDatePickerProps<
 export function ControlledDatePicker<T extends FieldValues>(props: IControlledDatePickerProps<T>) {
   const { field } = useController(props)
 
+  const onDateChange = (date: Date) => {
+    const event = {
+      target: {
+        name: field.name,
+        value: date
+      }
+    } as unknown as React.ChangeEvent<HTMLInputElement>;
+    field.onChange(event);
+  }
+
   return (
     <FormControl isRequired>
       <FormLabel>{props.label}</FormLabel>
       <SingleDatepicker
         name={props.name}
-        onDateChange={field.onChange}
+        onDateChange={onDateChange}
         date={field.value}
       />
     </FormControl>
